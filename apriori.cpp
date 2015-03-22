d88a57822ed54949a780f4117c7f8b9c66edc2eb
Naing <na0126dud@gmail.com>
2015-03-23 07:51:43 +0900

    find apriori candidates

--- apriori.cpp
+++ apriori.cpp
@@ -19,7 +19,6 @@ int cnt_l_pre;
 int cnt_c = 0;
 int cnt_k;
 
-// int transaction[1000][30];
 vector<vector<int>> transaction;
 
 typedef struct _tid_List{
@@ -30,8 +29,11 @@ typedef struct _tid_List{
 vector<tid_list> vertical_type;
 int mapDataToVID[100000]; // mapping data to vertical type idx
 
-int L[100000][10];
-int C[100000][10];
+//int L[100000][10];
+vector<vector<vector<int>>> L;
+//int C[100000][10];
+vector<vector<vector<int>>> C;
+
 float sup[100000];
 
 int vertical[30][1000];
@@ -42,9 +44,6 @@ void init(){
 	int i, j;
 
 	for (i = 0; i < 100000; i++){
-		for (j = 0; j < 10; j++){
-			L[i][j] = C[i][j] = -1;
-		}
 		mapDataToVID[i] = -1;
 		sup[i] = 0;
 	}
@@ -83,145 +82,212 @@ void printVertical(){
 	printf("vertical type count is %d\n", vertical_type.size());
 }
 
-void findIntersection(vector<int> &target){
-	printf("find intersection which include ");
-	int i;
-	int targetSize = target.size();
-	printf("target array size is %d\n", targetSize);
-	for (i = 0; i < targetSize - 1; i++){
-		printf("%d, ", target[i]);
-	}
-	printf("%d\n", target[targetSize - 1]);
-}
 
-/*
-void printChk(){
-int i;
-printf("printChk\n");
-for (i = 0; i < 100000; i++)
-printf("%d", chk[i]);
-printf("\n");
-}
-*/
-
-/*
 void generateL1(){
-int i;
-printf("generateL1\n");
-//printChk();
-for (i = 0; i < 100000; i++){
-if (chk[i] == 1){
-L[cnt_l][0] = i;
-cnt_l++;
+	int i;
+	printf("generateL1\n");
+	vector<vector<int>> L1;
+	vector<vector<int>> C1;
+
+	for (i = 0; i < vertical_type.size() ; i++){
+		vector<int> temp;
+		temp.push_back(vertical_type[i].data);
+		C1.push_back(temp);
+		if (vertical_type[i].list.size()>=min_sup_num)
+			L1.push_back(temp);
+	}
+	sort(C1.begin(), C1.end());
+	C.push_back(C1);
+
+	sort(L1.begin(), L1.end());
+	L.push_back(L1);
+	
 }
-}
-cnt_l_pre = 0;
-}
-*/
 
 void printL(int k){
 	int i, j;
 	printf("printL%d\n", k);
-	for (i = 0; i < cnt_l; i++){
-		for (j = 0; j < k; j++){
-			printf("%d ", L[i][j]);
+	for (i = 0; i < L[k-1].size(); i++){
+		for (j = 0; j < L[k - 1][i].size(); j++){
+			printf("%d ", L[k-1][i][j]);
 		}
 		printf("\n");
 	}
 }
 
-int has_infrequent_subset(int cnt_c, int k){
-	// C[cnt_c]의 subset들이 L에 전부 있는지 확인. 없는게 있으면 1, 없으면 0
-	int ti, li, cki, lki;
-	int i;
-	int start = 0;
-	int hasInfrequent = 0;
-	int isSame = 0;
+void printC(int k){
+	int i, j;
+	printf("printC%d\n", k);
+	for (i = 0; i < C[k - 1].size(); i++){
+		for (j = 0; j < C[k - 1][i].size(); j++){
+			printf("%d ", C[k - 1][i][j]);
+		}
+		printf("\n");
+	}
+}
 
-	for (ti = 0; ti < k + 1; ti++){
-		hasInfrequent = 0;
-		for (li = 0; li < cnt_l; li++){
-			cki = lki = 0;
-			while (lki <= k){
-				if (cki == ti)
-					cki++;
-				if (C[cnt_c][cki] != L[li][lki]){
-					break;
-				}
-				cki++;
-				lki++;
-			}
-			if (lki > k){
-				//same
-				isSame = 1;
+bool isExist(vector<vector<int>> list, vector<int> target){
+	int i, j;
+	for (i = 0; i < list.size(); i++){
+		int flag = 1;
+		for (j = 0; j < target.size(); j++){
+			if (list[i][j] != target[j]){
+				flag = 0;
 				break;
 			}
 		}
-		if (isSame == 0){
-			return 1;
-		}
+		if (flag == 1)
+			return true;
 	}
+	return false;
 }
 
-void apriori_gen(int k){
+vector<vector<int>> join(int k){
+	// self-joining Lk and make Ck+1 candidates
+	printf("self-joining L%d\n", k);
+
 	int i, j;
-	int t = 0;
-	for (i = 0; i < cnt_l; i++){
-		for (j = i; j < cnt_l; j++){
-			while (L[i][t] == L[j][t]){
-				t++;
+	int ida = 0,idb = 0;
+
+	printf("L[%d]'s size is %d\n", k, L[k - 1].size());
+
+	vector<vector<int>> temp_c_list;
+
+	for (i = 0; i < L[k-1].size(); i++){
+		for (j = i + 1; j < L[k-1].size(); j++){
+			int dif_cnt = 0;
+			int dif_data;
+			int ida = 0;
+			int idb = 0;
+			int flag = 1; // 1 : enable, 0 : not enable
+			while (ida<k && idb<k){
+				if (L[k - 1][i][ida] != L[k - 1][j][idb]){
+					dif_cnt++;
+					if (dif_cnt > 1){
+						// when different data cnt is more than 1, join is not enable
+						flag = 0;
+						break;
+					}
+					if (L[k - 1][i][ida] < L[k - 1][j][idb]){
+						dif_data = L[k - 1][i][ida];
+						idb--;
+					}
+					else{
+						dif_data = L[k - 1][j][idb];
+						ida--;
+					}
+				}
+				ida++;
+				idb++;
 			}
-			if (t == k){
+			if (flag == 1){
 				// join
-				for (t = 0; t < k - 1; t++){
-					C[cnt_c][t] = L[i][t];
+				int join_id;
+				vector<int> temp_c;
+				vector<int> target;
+				if (ida == k){
+					target = L[k - 1][j];
 				}
-				if (L[i][k] < L[j][k]){
-					C[cnt_c][k] = L[i][k];
-					C[cnt_c][k + 1] = L[j][k];
+				else{
+					target = L[k - 1][i];
 				}
-				cnt_c++;
-				if (has_infrequent_subset(cnt_c, k)){
-					cnt_c--;
+				for (join_id = 0; join_id < k; join_id++){
+					temp_c.push_back(target[join_id]);
 				}
+				temp_c.push_back(dif_data);
+				sort(temp_c.begin(), temp_c.end());
+
+				if (!isExist(temp_c_list,temp_c))
+					temp_c_list.push_back(temp_c);
 			}
-			t = 0;
 		}
 	}
-
+	return temp_c_list;
 }
 
-void scanning(int cnt_k){
-	// counting 후 min_sup보다 큰 경우만 L에 저장
-	cnt_l = 0;
+bool pruning(int k, vector<vector<int>> temp_c_list){
+	// pruning Ck candidates
+	printf("pruning C%d\n", k);
 
-	int ci, ti, cki, tki;
-	for (ci = 0; ci < cnt_c; ci++){
-		for (ti = 0; ti < cnt_t; ti++){
-			for (cki = 0; cki < cnt_k; cki++){
-				tki = 0;
-				while (C[ci][cki] != transaction[ti][tki] && transaction[ti][tki] > -1){
-					tki++;
-				}
-				if (transaction[ti][tki] < 0 || tki>30){
-					// no exist
+	if (temp_c_list.size() == 0)
+		return false;
+
+	int C_id, i, L_id;
+	vector<vector<int>> pruned_c_list;
+
+	for (C_id = 0; C_id < temp_c_list.size(); C_id++){
+		int flag = 1;
+		for (i = 0; i < k - 1; i++){
+			vector<int> temp = temp_c_list[C_id];
+			temp.erase(temp.begin()+i);
+			for (L_id = 0; L_id < L[k - 2].size(); L_id++){
+				if (equal(temp.begin(),temp.end(),L[k-2][L_id].begin())){
 					break;
 				}
 			}
-			if (cki > cnt_k){
-				// exist
-				sup[ci]++;
+			if (L_id==L[k-2].size()){
+				flag = 0;
+				break;
 			}
 		}
-		if (sup[ci] > min_sup_num){
-			// add to L
-			int i;
-			for (i = 0; i < cnt_k; i++){
-				L[cnt_l][i] = C[ci][i];
-			}
-			cnt_l++;
+		if (flag == 1){
+			pruned_c_list.push_back(temp_c_list[C_id]);
 		}
 	}
+	
+	if (pruned_c_list.size() > 0){
+		C.push_back(pruned_c_list);
+		return true;
+	}
+	else
+		return false;
+}
+
+bool apriori_gen(int k){
+	return pruning(k, join(k - 1));
+}
+
+int findIntersection(vector<int> data_set){
+	int i;
+
+	vector<int> result = vertical_type[mapDataToVID[data_set[0]]].list;
+	
+	for (i = 1; i < data_set.size(); i++){
+		vector<int> temp(transaction.size());
+		vector<int>::iterator it;
+		vector<int> target = vertical_type[mapDataToVID[data_set[i]]].list;
+
+		it = set_intersection(result.begin(), result.end(), target.begin(), target.end(),temp.begin());
+		temp.resize(it - temp.begin());
+
+		result.clear();
+
+		for (it = temp.begin(); it != temp.end(); ++it){
+			result.push_back(*it);
+		}
+	}
+
+	return result.size();
+}
+
+void scanning(int k){
+	// Ck의 sup를 구하여 기준을 통과하는 애들만 Lk로 push
+	printf("scanning C%d\n",k);
+	int i;
+
+	vector<vector<int>> temp_L_list;
+
+	for (i = 0; i < C[k - 1].size(); i++){
+		int cnt = findIntersection(C[k - 1][i]);
+		int j;
+		printf("[");
+		for (j = 0; j < C[k - 1][i].size(); j++)
+			printf("%d ", C[k - 1][i][j]);
+		printf("] = %d\n",cnt);
+		if (cnt >= min_sup_num)
+			temp_L_list.push_back(C[k - 1][i]);
+	}
+	L.push_back(temp_L_list);
 }
 
 void printCandidates(){
@@ -280,40 +346,27 @@ int main(int argc, char* argv[]){
 		}
 	}
 
-	printTransaction();
+//	printTransaction();
 
 	printVertical();
 
 	min_sup = atoi(argv[1]);
-	min_sup_num = min_sup * cnt_t / 100;
+	min_sup_num = min_sup * transaction.size() / 100;
 
-	printf("min_sup is %d\n", min_sup_num);
+	printf("min_sup_num is %d\n", min_sup_num);
 
-	//generateL1();
-
-	//printL(1);
-
-	vector<int> target;
-	target.push_back(1);
-	target.push_back(2);
-	target.push_back(4);
-
-	printf("target size is %d\n", target.size());
-
-	findIntersection(target);
-	/*
+	generateL1();
+	
 	int k;
-	for (k = 2; cnt_l > 0;k++){
-	cnt_l_pre = cnt_l;
-	apriori_gen(k-1);
-	scanning(k);
+	for (k = 2; k < vertical_type.size() ;k++){
+		if (apriori_gen(k)){
+			printC(k);
+			scanning(k);
+			printL(k);
+		}
+		else
+			break;
 	}
-
-	cnt_k = k - 1;
-
-	printCandidates();
-
-	*/
 
 	// write to output file
 	output = fopen(argv[3], "w");
